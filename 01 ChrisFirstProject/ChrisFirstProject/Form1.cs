654f75a023477617e0a7638123df736f7bc5fa05
MajorPaine <cdpaine2003@yahoo.com>
2015-07-30 06:39:49 -0400

    First Project

---
+++ 01 ChrisFirstProject/ChrisFirstProject/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChrisFirstProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello","Chris Paine!");
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("You are hovering");
+        }
+    }
+}
